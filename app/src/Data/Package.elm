ab44976a6701e25445b819ea079e2fd3aff71fe6
Michael Torres <michael@notion.ai>
2017-11-02 20:31:39 -0400

    update package data type

--- app/src/Data/Package.elm
+++ app/src/Data/Package.elm
@@ -1,6 +1,21 @@
 module Data.Package exposing (Package)
 
 
+type Category
+    = List
+    | Dict
+    | Set
+    | Array
+    | Queue
+    | Stack
+    | Matrix
+    | Graph
+    | Tree
+    | Tuple
+    | Other
+
+
 type alias Package =
     { name : String
+    , link : String
     }
