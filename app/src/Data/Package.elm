5992e4a63c7583aef21bca7774e7eb33ea7652b7
Anthony Deschamps <anthony.deschamps@soarai.com>
2017-11-02 21:41:40 -0400

    Add list of properties to the Package type.

--- app/src/Data/Package.elm
+++ app/src/Data/Package.elm
@@ -39,4 +39,14 @@ type alias Package =
     { name : String
     , link : String
     , category : Category
+
+    -- Properties
+    -- TODO: These are Maybe types because we don't want to fill in this information for all the data structures right now.
+    -- Once all that information is filled in, these can be replaced with Bools
+    , ordered : Maybe Bool
+    , preservesOrder : Maybe Bool
+    , uniqueElements : Maybe Bool
+    , uniqueIdentifiers : Maybe Bool
+    , keyValue : Maybe Bool
+    , prioritized : Maybe Bool
     }
