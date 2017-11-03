2003882a15e1eaf17a3af4a04773f84342dd1e15
Anthony Deschamps <anthony.deschamps@soarai.com>
2017-11-02 21:39:45 -0400

    Add list of categories.

--- app/src/Data/Package.elm
+++ app/src/Data/Package.elm
@@ -1,4 +1,4 @@
-module Data.Package exposing (Package, Category(..))
+module Data.Package exposing (Package, Category(..), categories)
 
 
 type Category
@@ -14,6 +14,25 @@ type Category
     | Tuple
     | CRDT
     | Other
+
+
+{-| List of all categories, useful for iterating over
+-}
+categories : List Category
+categories =
+    [ List
+    , Dict
+    , Set
+    , Array
+    , Queue
+    , Stack
+    , Matrix
+    , Graph
+    , Tree
+    , Tuple
+    , CRDT
+    , Other
+    ]
 
 
 type alias Package =
