af9ab9a1836f9a76ccbd099891fde9088a974d76
Michael Torres <torresmi@mail.gvsu.edu>
2018-01-01 13:21:47 -0500

    add main css class

--- app/src/Views/Colors.elm
+++ app/src/Views/Colors.elm
@@ -4,3 +4,8 @@ module Views.Colors exposing (..)
 primaryColorClass : String
 primaryColorClass =
     "cyan lighten-1"
+
+
+backgroundColorClass : String
+backgroundColorClass =
+    "grey lighten-3"
--- app/src/Views/Page.elm
+++ app/src/Views/Page.elm
@@ -2,11 +2,12 @@ module Views.Page exposing (frame)
 
 import Html exposing (Html)
 import Html.Attributes as Attrs
+import Views.Colors as Colors
 
 
 frame : Html msg -> Html msg -> Html msg
 frame navbarView content =
-    Html.div []
+    Html.div [ Attrs.class <| "main-frame " ++ Colors.backgroundColorClass ]
         [ navbarView
         , Html.div [ Attrs.class "page-frame" ]
             [ content
